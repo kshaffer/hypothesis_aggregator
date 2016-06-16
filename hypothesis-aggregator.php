3c2dace6dfe468b5b4c5e13ce15d9ebd752bc1ea
Kris Shaffer <kris.shaffer@gmail.com>
2016-06-16 12:21:21 -0400

    user, tags, and text options now working

--- hypothesis-aggregator.php
+++ hypothesis-aggregator.php
@@ -45,59 +45,87 @@ class HypothesisAPI {
 	}
 }
 
-function hypothesis_shortcode() {
+function hypothesis_shortcode( $atts ) {
+		$hypothesis_atts = shortcode_atts(array(
+				'user' => '',
+				'tags' => '',
+				'text' => ''
+		), $atts);
 		$output = '';
 		$hypothesis = new HypothesisAPI();
-		$search_results = $hypothesis->search(array('user' => 'kris.shaffer'));
-		foreach($search_results as $key => $value) {
-				// store annotation locally
-				$annotation_local = $hypothesis->read($value->id);
-
-				// title of post with link to original post
-				$output .= '<a href="';
-				$output .= $annotation_local->uri;
-				$output .= '">';
-				$output .= $annotation_local->document->title[0];
-				$output .= '</a>';
-				$output .= '<br/><br/>';
-
-				// grab highlighted portion of post
-				if (in_array('selector', array_keys(get_object_vars($annotation_local->target[0])))) {
-						$selector = $annotation_local->target[0]->selector;
-						foreach($selector as $entry => $value) {
-								if (in_array('exact', array_keys(get_object_vars($value)))) {
-										$target_info = $value->exact;
-								} else {
-										$target_info = 'No highlighted text in this annotation.';
-								}
+		if ($hypothesis_atts['user'] == '' && $hypothesis_atts['tags'] == '' && $hypothesis_atts['text'] == '') {
+				return '<p>No search terms provided.</p>';
+		} else {
+				if ($hypothesis_atts['user'] == '') {
+						if ($hypothesis_atts['tags'] == '') {
+								$search_results = $hypothesis->search(array('text' => $hypothesis_atts['text']));
+						} elseif ($hypothesis_atts['text'] == '') {
+								$search_results = $hypothesis->search(array('tags' => $hypothesis_atts['tags']));
+						} else {
+								$search_results = $hypothesis->search(array('text' => $hypothesis_atts['text'], 'tags' => $hypothesis_atts['tags']));
 						}
 				} else {
-						$target_info = 'No highlighted text in this annotation.';
+						if ($hypothesis_atts['tags'] == '' && $hypothesis_atts['text'] == '') {
+								$search_results = $hypothesis->search(array('user' => $hypothesis_atts['user']));
+						} elseif ($hypothesis_atts['tags'] == '') {
+								$search_results = $hypothesis->search(array('user' => $hypothesis_atts['user'], 'text' => $hypothesis_atts['text']));
+						} elseif ($hypothesis_atts['text'] == '') {
+								$search_results = $hypothesis->search(array('user' => $hypothesis_atts['user'], 'tags' => $hypothesis_atts['tags']));
+						} else {
+								$search_results = $hypothesis->search(array('user' => $hypothesis_atts['user'], 'text' => $hypothesis_atts['text'], 'tags' => $hypothesis_atts['tags']));
+						}
 				}
 
-				// highlighted portion of post as blockquote
-				$output .= '<blockquote>';
-				$output .= $target_info;
-				$output .= '</blockquote><br/>';
+				foreach($search_results as $key => $value) {
+						// store annotation locally
+						$annotation_local = $hypothesis->read($value->id);
 
-				// annotation comment, with username and link to stream
-				if ($annotation_local->text) {
-						$output .= $annotation_local->text;
-						$output .= '<br/>';
+						// title of post with link to original post
+						$output .= '<a href="';
+						$output .= $annotation_local->uri;
+						$output .= '">';
+						$output .= $annotation_local->document->title[0];
+						$output .= '</a>';
+						$output .= '<br/><br/>';
 
+						// grab highlighted portion of post
+						if (in_array('selector', array_keys(get_object_vars($annotation_local->target[0])))) {
+								$selector = $annotation_local->target[0]->selector;
+								foreach($selector as $entry => $value) {
+										if (in_array('exact', array_keys(get_object_vars($value)))) {
+												$target_info = $value->exact;
+										} else {
+												$target_info = 'No highlighted text in this annotation.';
+										}
+								}
+						} else {
+								$target_info = 'No highlighted text in this annotation.';
+						}
+
+						// highlighted portion of post as blockquote
+						$output .= '<blockquote>';
+						$output .= $target_info;
+						$output .= '</blockquote><br/>';
+
+						// annotation comment, with username and link to stream
+						if ($annotation_local->text) {
+								$output .= $annotation_local->text;
+								$output .= '<br/><br/>';
+
+						}
+						$account = $annotation_local->user;
+						$output .= 'Curated by <a href="';
+						$output .= "https://hypothes.is/stream?q=user:";
+						list($dump1, $account_name, $dump2) = split('[:@]', $account);
+						$output .= $account_name;
+						$output .= '">';
+						$output .= $account_name;
+						$output .= '</a>.<br/>';
+
+						$output .= "<br/><hr/><br/>";
 				}
-				$account = $annotation_local->user;
-				$output .= 'Curated by <a href="';
-				$output .= "https://hypothes.is/stream?q=user:";
-				list($dump1, $account_name, $dump2) = split('[:@]', $account);
-				$output .= $account_name;
-				$output .= '">';
-				$output .= $account_name;
-				$output .= '</a>.<br/>';
-
-				$output .= "<br/><hr/><br/>";
+				return $output;
 		}
-		return $output;
 }
 
 function hypothesis_register_shortcode() {
